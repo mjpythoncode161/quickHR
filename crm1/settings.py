41b0717b2f144fdd2aa19347b8de26e891431b33
devndataai <mjpythoncode@gmail.com>
2026-07-06 14:43:35 +0530

    12
    12

--- crm1/settings.py
+++ crm1/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,7 +35,6 @@ ALLOWED_HOSTS = [
     "147.93.107.11",
     "quickhr.onrender.com",
     ".onrender.com",
-    "https://quickhr.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -99,15 +100,23 @@ WSGI_APPLICATION = "crm1.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "OPTIONS": {
-            "timeout": 30,  
-        },
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "OPTIONS": {
+                "timeout": 30,
+            },
+        }
+    }
 
 
 # Password validation
